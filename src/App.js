be57400753479b30e4be9970a93ff38adbfad3ab
ZooHairDev <zouhair.ob@gmail.com>
2023-12-27 23:04:35 +0100

    Made components & Added Meta tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div>Homepage</div>
+    <>
+    <head>
+      <title>Little Lemon Restaurant</title>
+      <meta name="description" content="Little Lemon Restaurant."/>
+      <meta name="og:title" content="Little Lemon Restaurant." />
+      <meta name="og:description" content="Mediteraenian restaurant for families." />
+      <meta name="og:image" content="Login.svg" />
+    </head>
+    <Header />
+    <Nav />
+    <Main>Homepage</Main>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+    <nav>
+        <ul>
+            <li><a href="/home">HOME</a></li>
+            <li><a href="/about">ABOUT</a></li>
+            <li><a href="/menu">MENU</a></li>
+            <li><a href="/reservations">RESERVATIONS</a></li>
+            <li><a href="/order_online">ORDER ONLINE</a></li>
+            <li><a href="/login">LOG IN</a></li>
+        </ul>
+    </nav>
+    );
+}
+
+export default Nav;
